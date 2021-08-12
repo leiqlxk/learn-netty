b61f5587b32ede7bc0787b6d1372af717cd85a7f
leiqlxk <472990991@qq.com>
2021-08-12 22:09:53 +0800

    half blag

---
+++ src/main/java/org/lql/netty/advance/halfblag/DelimiterServer.java
@@ -0,0 +1,50 @@
+package org.lql.netty.advance.halfblag;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * Title: DelimiterServer <br>
+ * ProjectName: learn-netty <br>
+ * description: TODO <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/8/12 21:32 <br>
+ */
+public class DelimiterServer {
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            ChannelFuture future = new ServerBootstrap()
+                    .group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel channel) throws Exception {
+                            // 分割符解码器，也可以解决黏包半包问题，但其需要一个字节一个字节解析，效率也会有影响
+                            channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+                            channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        }
+                    }).bind(9000);
+
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/org/lql/netty/advance/halfblag/HelloWorldServer.java
@@ -0,0 +1,52 @@
+package org.lql.netty.advance.halfblag;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Title: HelloWorldServer <br>
+ * ProjectName: learn-netty <br>
+ * description: TODO <br>
+ *
+ * @author: leiql <br>
+ * @version: 1.0 <br>
+ * @since: 2021/8/12 20:07 <br>
+ */
+public class HelloWorldServer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldServer.class);
+
+    public static void main(String[] args) {
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+
+        try {
+            ChannelFuture future = new ServerBootstrap()
+                    .group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    // 设置系统的接收缓冲区（滑动窗口）的大小，不设置的话客户端和服务端会自己协商设置合适大小，不设置黏包，设置后半包
+//                    .option(ChannelOption.SO_RCVBUF, 10)
+                    // 设置netty的接收缓冲区，即应用层的缓冲区，netty默认是1024，内部规则为16的整数倍，设置过小容易出现半包
+                    .childOption(ChannelOption.RCVBUF_ALLOCATOR, new AdaptiveRecvByteBufAllocator(16, 16, 16))
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel channel) throws Exception {
+                            channel.pipeline().addLast(new LoggingHandler(LogLevel.DEBUG));
+                        }
+                    }).bind(9000);
+
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+        }
+    }
+}
